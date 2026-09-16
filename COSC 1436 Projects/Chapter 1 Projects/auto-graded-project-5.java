ce5bb422f4427c61a96bc97d95bf77bffc1b9a9c
autopia-cyber <aymen.labidi.cs@gmail.com>
2026-09-16 16:13:37 -0500

    Create auto-graded-project-5.java

---
+++ COSC 1436 Projects/Chapter 1 Projects/auto-graded-project-5.java
@@ -0,0 +1,14 @@
+public class Exercise {
+
+    public static void main (String[] args) {
+        double v0 = 5.6;
+        double v1 = 10.5; 
+        double t = 0.5;
+
+        double acceleration = ( v1 - v0 ) / t;
+
+        System.out.println ("The average acceleration is " + acceleration );
+  
+    }
+
+}
