47657c95330b3fa74dd85c0a57be379b854ce911
autopia-cyber <aymen.labidi.cs@gmail.com>
2026-09-16 16:11:49 -0500

    Add Exercise class to calculate discriminant

---
+++ COSC 1436 Projects/Chapter 1 Projects/auto-graded-project-4.java
@@ -0,0 +1,14 @@
+public class Exercise {
+
+    public static void main (String[] args) {
+        double a = 3;
+        double b = 4;
+        double c = 5;
+
+        double discriminant = b * b -4 * a * c;
+
+        System.out.println("The discriminant is " + discriminant);
+
+    }
+  
+}
